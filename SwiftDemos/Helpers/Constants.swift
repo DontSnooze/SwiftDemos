d394c5e2e73f3a99f496243a468b5062bb01c55b
amos <amos@dontsnooze.com>
2025-03-04 17:58:25 -0500

    Addressed network issue with hub.dummyapis.com
    - hub.dummyapis.com has a certificate issue and won't load on sim causing the pipeline to fail. Using link to Employees.json on the repo for now
    - also fixes UI Tests

--- SwiftDemos/Helpers/Constants.swift
+++ SwiftDemos/Helpers/Constants.swift
@@ -9,7 +9,14 @@ import Foundation
 
 struct Constants {
     struct Urls {
-        static let employeesUrl = URL(string: "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001")!
+        
+        // hub.dummyapis.com has a certificate issue and won't load on sim.
+        // Which means it wont work in the CICD pipeline on sim
+        // Using link to Employees.json on the gh repo for now
+        // static let employeesUrl = URL(string: "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001")!
+        
+        static let employeesUrl = URL(string: "https://github.com/DontSnooze/SwiftDemos/raw/refs/heads/main/SwiftDemos/Helpers/Employees.json")!
+    
         static let employeeUrl = URL(string: "https://hub.dummyapis.com/employee?noofRecords=1&idStarts=1001")!
         static let singleListUrl = URL(string: "https://hub.dummyapis.com/singlelistwithid?text=Test&noofRecords=10")!
     }
